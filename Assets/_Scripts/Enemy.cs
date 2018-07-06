a0738895eabdcd143d35178d0e418f2a96c663f3
JFU5 <JFU5@CILABS-114B-10.scilsnet.rutgers.edu>
2018-07-05 20:21:48 -0400

    Enemy now blinks red when damaged.
    TODO: Adding Power-Ups and Boosting
    Weapons

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+
     [Header("Set in Inspector: Enemy")]
     public float speed = 10f;
     public float fireRate = 0.3f;
     public float health = 10;
     public int score = 100;
+    public float showDamageDuration = 0.1f;
+
+    [Header("Set Dynamically: Enemy")]
+    public Color[] originalColors;
+    public Material[] materials;
+    public bool showingDamage = false;
+    public float damageDoneTime;
+    public bool notifiedOfDestruction = false;
 
     protected BoundsCheck bndCheck;
 
     void Awake() {
         bndCheck = GetComponent<BoundsCheck>();
+        materials = Utils.GetAllMaterials(gameObject);
+        originalColors = new Color[materials.Length];
+        for (int i = 0; i < materials.Length; i++) {
+            originalColors[i] = materials[i].color;
+        }
     }
 
     public Vector3 pos {
@@ -26,6 +40,10 @@ public class Enemy : MonoBehaviour {
 
     void Update() {
         Move();
+
+        if (showingDamage && Time.time > damageDoneTime) {
+            UnShowDamage();
+        }
 
         if (bndCheck != null && bndCheck.offDown) {
                 Destroy(gameObject);
@@ -47,7 +65,8 @@ public class Enemy : MonoBehaviour {
                     Destroy(otherGO);
                     break;
                 }
-                health -= Main.GetWeaponDefinition(p.type).damageOnHit;
+                ShowDamage();
+                health -= Main.GetWeaponDefinition(p.type).damageOnHit;
                 if (health <= 0) {
                     Destroy(this.gameObject);
                 }
@@ -57,5 +76,20 @@ public class Enemy : MonoBehaviour {
                 print("Enemy hit by non-ProjectileHero: " + otherGO.name);
                 break;
         }
+    }
+
+    void ShowDamage() {
+        foreach (Material m in materials) {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage() {
+        for (int i = 0; i < materials.Length; i++) {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
     }
 }
--- Assets/_Scripts/Utils.cs
+++ Assets/_Scripts/Utils.cs
@@ -9,10 +9,23 @@ public enum BoundsTest {
 	offScreen 		//are bounds entirely off screen
 }
 
-public class Utils : MonoBehaviour
-{
-	// create bounds that expand to hold the two bounds passed in
-	public static Bounds BoundsUnion( Bounds b0, Bounds b1) {
+public class Utils : MonoBehaviour {
+
+    //============================ Materials Functions ===========================\\
+    // Returns a list of all Materials on this GameObject and its children
+    static public Material[] GetAllMaterials(GameObject go) {
+        Renderer[] rends = go.GetComponentsInChildren<Renderer>();
+
+        List<Material> mats = new List<Material>();
+        foreach (Renderer rend in rends) {
+            mats.Add(rend.material);
+        }
+
+        return (mats.ToArray());
+    }
+
+    // create bounds that expand to hold the two bounds passed in
+    public static Bounds BoundsUnion( Bounds b0, Bounds b1) {
 		if (b0.size == Vector3.zero && b1.size != Vector3.zero) {
 			return (b1);
 		} else if (b0.size != Vector3.zero && b1.size == Vector3.zero) {
@@ -191,9 +204,6 @@ public class Utils : MonoBehaviour
 		return (Vector3.zero);  // if we get here something went wrong
 	
 	} // end BoundsInBoundsCheck
-	
-	
-	
 }// End of Util Class
 
 
